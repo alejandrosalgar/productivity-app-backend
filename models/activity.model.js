6bd37c44c3ae59a41bfda7b2a91b3607b257d815
Alejandro Salgar Marín <alejandro.salgar@gmail.com>
2023-05-01 20:55:13 -0500

    Update activity.model.js

--- models/activity.model.js
+++ models/activity.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 /* Creating a new schema for the activity model. */
 const activitySchema = new Schema({
-  name: {
+  nameSA: {
     type: String,
     required: true,
   },
